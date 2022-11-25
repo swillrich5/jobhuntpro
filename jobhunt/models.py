60e8ebc78db983beeb20597c966c7a21150f2fa9
Scott Willrich <swillrich@gmail.com>
2022-11-24 21:31:26 -0600

    added Contact and Activity tables

--- jobhunt/models.py
+++ jobhunt/models.py
@@ -12,3 +12,18 @@ class Job(models.Model):
 
     def __str__(self):
         return f'self.job_title + ": " + self.employer'
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=11)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+class Activity(models.Model):
+    activity = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    note = models.TextField()
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+
